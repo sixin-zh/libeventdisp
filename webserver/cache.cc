f3037a34e8e6b5b2e4e7cd79181ee44a5fd3e48a
renctan@gmail.com <renctan@gmail.com@9c0f7339-69f1-b846-8184-f6f87cd86ce0>
2011-04-22 19:31:12 +0000

    Implemented stubs for Cache

---
+++ webserver/cache.cc
@@ -0,0 +1,15 @@
+#include "cache.h"
+
+namespace nyu_libedisp_webserver {
+Cache::Cache(size_t sizeQuota) : sizeQuota_(sizeQuota) {
+}
+
+bool Cache::put(const char *key, char *buf, size_t size) {
+  return false;
+}
+
+bool Cache::get(const char *key, char **buff, size_t &size) {
+  return false;
+}
+}
+
